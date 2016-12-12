75eacc44c5be9dcdb261c8d8e81e4070a7582671
sakopyan <SAAkopyan@gmail.com>
2016-12-12 22:20:01 +0300

    Отредактированный сиснтаксис PolindromCheker!

--- Simple_examples/PolindromChecker.java
+++ Simple_examples/PolindromChecker.java
@@ -1,20 +1,19 @@
-package FromStepik;
 
 import java.util.Scanner;
 
 /**
- * Created by SAkopyan on 30.09.16.
+ * Created by SAkopyan on 30.10.16.
  * Program name: PolindromChecker
  * Version: 1.0
  * Description:
- * Задача данной программы заключается в считывании из стандартного ввода предложения и его проверки на полиандромность.
+ * Задача данной программы заключается в считывании из стандартного ввода предложения и его проверки на полиндромность.
  * В качестве результата выводится сообщение в виде булевого значения True or False.
  */
 public class PolindromChecker {
     public static void main(String[] args){
-        //---Блок кода реализующий конструкцию (обьект) для считывания ввода из консоли
+        //---Блок кода реализующий конструкцию для считывания ввода из консоли
         Scanner in = new Scanner(System.in);
-        System.out.print("Введите предложение :");
+        System.out.print("Введите предложение:");
         String text = in.nextLine();
         //---System.out.println(text);
 
@@ -23,7 +22,6 @@ public class PolindromChecker {
         //---
 
         //---Метод replaceAll класса String позволяющий производить замену символов в строке по регулярному выражению
-        // (в нашем случае [^a-zA-Z0-9] просто заменяется на "" пусто)
         String text2 = text.replaceAll("[^a-zA-Z0-9]", "");
         //---
 
@@ -34,10 +32,10 @@ public class PolindromChecker {
 
         //Вывод программы:
 
-        System.out.println("Общее количество символов в предложении : " + length + "последний символ это: " + text.charAt(length-1));
-        System.out.println("Модифицированная строка :" + text2);
-        System.out.println("Обратная модифицированная строка :" + text3);
-        System.out.print("Результат проверки строки на полиандромность :" + text2.equalsIgnoreCase(text3));
+        System.out.println("Общее количество символов в предложении: " + length + " последний символ строки: " + text.charAt(length-1));
+        System.out.println("Модифицированная строка: " + text2);
+        System.out.println("Перевернутая строка: " + text3);
+        System.out.println("Результат проверки строки на полиандромность: " + text2.equalsIgnoreCase(text3));
 
     }
 }
